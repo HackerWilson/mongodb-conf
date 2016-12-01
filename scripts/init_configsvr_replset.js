8155bc3c89af576c30dbb98d7c88a2437d57c923
HackerWilson <wenbinwb1@foxmail.com>
2016-12-01 16:07:36 +0800

    部署mongodb3.4

--- scripts/init_configsvr_replset.js
+++ scripts/init_configsvr_replset.js
@@ -1,9 +1,8 @@
 var result = rs.initiate({
-                 "_id" : "replset",
+                 "_id" : "configReplSet",
+                 "configsvr": true,
                  "members" : [
-                     { "_id" : 1, "host" : "127.0.0.1:27027"},
-                     { "_id" : 2, "host" : "127.0.0.1:27028"},
-                     { "_id" : 3, "host" : "127.0.0.1:27029"},
+                     { "_id" : 1, "host" : "127.0.0.1:27019"},
                  ]});
 
 if(result["ok"] == 0){
--- scripts/init_sharding.js
+++ scripts/init_sharding.js
@@ -1,12 +1,11 @@
-sh.addShard("127.0.0.1:27018");
-
 sleep(3000) /* wait replica set be ready.*/
+
+sh.addShard("127.0.0.1:27018");
 var result = sh.addShard("replset/127.0.0.1:27027,127.0.0.1:27028,127.0.0.1:27029");
-if(result["ok"] == 0)
-{
+if(result["ok"] == 0){
     print("Error: " + result["errmsg"]);
 }
-else
-{
+else{
     print("========> Init shard cluster [sharding] success! <========");
 }
+print();
